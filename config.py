53041cb9b8e779f405856005ccbcc819cea7549a
Falk Alexander Seidl <fa@terminal.run>
2019-12-25 22:35:24 +0100

    Add Sane Shaping Exceptions Destinations to Configuration

--- config.py
+++ config.py
@@ -29,7 +29,19 @@ DNS_SERVER = "1.1.1.1" # 141.46.140.31
 DAILY_TOPUP_VOLUME = 5368709120 # 5 GiB / in bytes
 MAX_SAVED_VOLUME = 37580963840 # 35 GiB / in bytes
 SHAPING_SPEED = "64kbit"
-SHAPING_EXCEPTIONS = ["192.168.100.150", "192.168.100.201", "88.198.248.254"]
+SHAPING_EXCEPTIONS = ["192.168.100.150",        # Local
+                    "192.168.100.201",          # Local
+                    "141.46.140.31",            # DNS Görlitz
+                    "141.46.14.31",             # DNS Zittau
+                    "141.46.8.0/24",            # HSZG network (public)
+                    "141.46.14.0/24",           # HSZG network (public)
+                    "141.46.21.0/24",           # HSZG network (public)
+                    "141.46.254.0/24",          # HSZG network (public)
+                    "141.46.141.0/24",          # HSZG network (internal)
+                    "134.109.133.26",           # OPAL
+                    "141.0.22.231",             # SWDD web
+                    "141.30.0.0/16",            # TUD (public)
+                    "141.76.0.0/16"]            # TUD (public)
 MAX_MAC_ADDRESSES_PER_REG_KEY = 5
 IP_RANGE_START = "10.90.0.2"
 IP_RANGE_END = "10.90.0.254"
