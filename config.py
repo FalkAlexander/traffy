99a3e5bd25fde76b5cc2414f6e569f51759c3737
Falk Alexander Seidl <fa@terminal.run>
2020-01-08 22:39:22 +0100

    Update Shaping Exceptions
    Use proper ip networks and subnets for our exempted organizations.

--- config.py
+++ config.py
@@ -26,24 +26,23 @@ DNSMASQ_HOSTS_FILE = "/home/traffy/dnsmasq/dnsmasq-hosts.conf"
 DNSMASQ_LEASE_FILE = "/home/traffy/dnsmasq/dnsmasq.leases"
 DNSMASQ_LISTEN_INTERFACE = "enp7s0"
 BRIDGE_INGRESS_INTERFACE = "ifb0"
-DNS_SERVER = "1.1.1.1" # 141.46.140.31
+DNS_SERVER = "141.46.140.31" # 141.46.140.31
 DAILY_TOPUP_VOLUME = 5368709120 # 5 GiB / in bytes
 MAX_SAVED_VOLUME = 37580963840 # 35 GiB / in bytes
 SHAPING_SPEED = "64kbit"
 SHAPING_EXCEPTIONS = ["192.168.100.150",        # Local
                     "192.168.100.201",          # Local
-                    "141.46.140.31",            # DNS Görlitz
-                    "141.46.14.31",             # DNS Zittau
-                    "141.46.8.0/24",            # HSZG network (public)
-                    "141.46.14.0/24",           # HSZG network (public)
-                    "141.46.21.0/24",           # HSZG network (public)
-                    "141.46.254.0/24",          # HSZG network (public)
-                    "141.46.141.0/24",          # HSZG network (internal)
-                    "134.109.133.26",           # OPAL
+                    "141.46.0.0/16",            # HSZG
+                    "134.109.0.0/16",           # TU Chemnitz / OPAL
                     "141.0.22.231",             # SWDD web
                     "141.30.0.0/16",            # TUD (public)
                     "141.76.0.0/16",            # TUD (public)
-                    "1.1.1.1"]
+                    "193.174.102.0/23",         # GR-NET
+                    "193.174.8.0/24",           # GR-NET
+                    "195.37.50.0/24",           # GR-NET
+                    "195.37.162.0/24",          # GR-NET
+                    "141.56.0.0/16",            # HTW
+                    "141.43.0.0/16"]            # B-TU
 MAX_MAC_ADDRESSES_PER_REG_KEY = 5
 IP_RANGE_START = "10.90.0.2"
 IP_RANGE_END = "10.90.0.254"
