58a2429639936bd7d2893d746ffa4a7b31cec12b
Falk Alexander Seidl <hi@falsei.de>
2020-05-24 17:17:03 +0200

    Cleanup

--- traffy-server/app/api/server.py
+++ traffy-server/app/api/server.py
@@ -1,3 +1,22 @@
+"""
+ Copyright (C) 2020 Falk Seidl <hi@falsei.de>
+ 
+ Author: Falk Seidl <hi@falsei.de>
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation; either version 2 of the
+ License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, see <http://www.gnu.org/licenses/>.
+"""
+
 from app.exceptions.user_exceptions import RegistrationError, DatabaseError, DeregistrationError
 from ..models import RegistrationKey, IpAddress, MacAddress, AddressPair, Traffic, Identity
 from ..util import arp_manager, generate_registration_key, lease_parser, iptables_rules_manager, iptables_accounting_manager, shaping_manager
--- traffy-server/app/tests/dev_mode.py
+++ traffy-server/app/tests/dev_mode.py
@@ -1,3 +1,22 @@
+"""
+ Copyright (C) 2020 Falk Seidl <hi@falsei.de>
+ 
+ Author: Falk Seidl <hi@falsei.de>
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation; either version 2 of the
+ License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, see <http://www.gnu.org/licenses/>.
+"""
+
 from ..models import RegistrationKey, Traffic, Identity, IpAddress
 from ..util import generate_registration_key
 from datetime import datetime
