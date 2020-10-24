d49ef8ae2f12785f05e4508b63c9b868d30b0544
Falk Alexander Seidl <hi@falsei.de>
2020-10-24 13:32:12 +0200

    Revert debug change from prerouting forwarding rule

--- traffy-server/app/util/nftables_manager.py
+++ traffy-server/app/util/nftables_manager.py
@@ -142,7 +142,7 @@ def add_ip_to_reg_key_set(ip_address, reg_key_id):
 # Captive Portal
 
 def insert_captive_portal_chain_forwarding_rules():
-    cmd = "insert rule ip traffy prerouting ip saddr @registered jump captive-portal"
+    cmd = "insert rule ip traffy prerouting ip saddr != @registered jump captive-portal"
     __execute_command(cmd)
 
 def add_unregistered_exception_accept_rules():
